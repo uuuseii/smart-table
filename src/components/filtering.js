c9b4ffabb295dc2458d0206fedeca6510f9dfe8c
uuuseii <uuuseii@gmail.com>
2025-11-17 02:24:44 +0300

    finish

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,33 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                        const newOption = document.createElement('option');
+                        newOption.value = name;
+                        newOption.textContent = name;
+                        return newOption;                             // @todo: создать и вернуть тег опции
+                      })
+        )
+     })
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-
+        /* if (action.target.tagName === 'button' && action.target.textContent === 'clear') {
+            action.target.parrentElement.children.forEach(element => {
+                if (element.tagName === 'input') {
+                    element.value = '';
+                }
+            })
+        } */
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -10,14 +10,33 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
+    before.reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container);
+    });
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => onAction());
+    root.container.addEventListener('reset', () => setTimeout``(onAction));
+    root.container.addEventListener('submit', e => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
